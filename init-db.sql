4a202aa56ef9bda1fa97637b91f6499d1ae99411
Aaron N. Brock <Contact@AaronNBrock.com>
2018-12-10 02:04:33 -0500

    Increased date precision

--- init-db.sql
+++ init-db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS authors (
     id serial PRIMARY KEY,
     name varchar(100) NOT NULL,
     email varchar(150) NOT NULL,
-    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
 );
 
 CREATE TABLE IF NOT EXISTS posts (
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS posts (
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
     author_id INTEGER REFERENCES authors(id),
-    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
 );
