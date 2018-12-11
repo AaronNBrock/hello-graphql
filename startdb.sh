392ae33230c2c5b1986761bece13cd85e257d660
Aaron N. Brock <Contact@AaronNBrock.com>
2018-12-10 19:50:01 -0500

    Added better windows support

--- startdb.sh
+++ startdb.sh
@@ -3,7 +3,7 @@ docker stop testdb || true
 docker run --rm --name testdb -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres:10.6
 
 # Wait for connection
-docker run --rm --network "host" --entrypoint /bin/sh jbergknoff/postgresql-client -c "while ! pg_isready -h localhost; do sleep 2; done"
+docker run --rm --network "host" --entrypoint sh jbergknoff/postgresql-client -c "while ! pg_isready -h localhost; do sleep 2; done"
 
 # Create User
 docker run --rm --network "host" jbergknoff/postgresql-client postgresql://postgres:password@localhost:5432 -c "CREATE USER graphql WITH password 'password';"
